37a890d2284844c03dd8efd4ec52a39c86c686fe
Edward Yuan <edward.yuan@iotpay.ca>
2022-09-01 01:00:05 -0700

    react template

--- gou-go-client/src/index.js
+++ gou-go-client/src/index.js
@@ -1,17 +1,44 @@
+/*!
+
+=========================================================
+* Argon Design System React - v1.1.1
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/argon-design-system-react
+* Copyright 2022 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/argon-design-system-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+
+import 'assets/vendor/nucleo/css/nucleo.css';
+import 'assets/vendor/font-awesome/css/font-awesome.min.css';
+import 'assets/scss/argon-design-system-react.scss?v1.1.0';
+
+import Index from 'views/Index.js';
+import Landing from 'views/examples/Landing.js';
+import Login from 'views/examples/Login.js';
+import Profile from 'views/examples/Profile.js';
+import Register from 'views/examples/Register.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' exact element={<Index />} />
+      <Route path='/landing-page' exact element={<Landing />} />
+      <Route path='/login-page' exact element={<Login />} />
+      <Route path='/profile-page' exact element={<Profile />} />
+      <Route path='/register-page' exact element={<Register />} />
+    </Routes>
+  </BrowserRouter>
+);
